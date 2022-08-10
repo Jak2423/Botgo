58e7671ceb5a07d2fce29077bd349430a0bf8774
Javkhlan <javhlan0420@gmail.com>
2022-08-10 19:26:19 +0800

    created sitemap.xml.js

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,36 @@
+const createSitemap = (id) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${id
+          .map((id) => {
+            return `
+                <url>
+                    <loc>${`https://botgo.vercel.app/${id}`}</loc>
+                </url>
+            `;
+          })
+          .join('')}
+    </urlset>
+`;
+export async function getServerSideProps({ res }) {
+  const allCourses = (await import('../lib/courses.json')).default;
+  const allPages = [
+    ...allCourses.map((course) => `courses/${course.id}`),
+    ...['', 'courses'],
+  ];
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=1200, stale-while-revalidate=600',
+  );
+  res.write(createSitemap(allPages));
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
