5685f773017e3154e60c5a3ee2b72ade89f687af
Javkhlan <javhlan0420@gmail.com>
2022-08-18 14:33:53 +0800

    some changes

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,11 @@ export async function getStaticProps() {
 
 export default function Home({ courses }) {
   const [search, setSearch] = useState('');
-  const [filteredCourses, setFilteredCourses] = useState([]);
-
-  const filterCourse = (value) => {
-    setSearch(value);
-    const result = courses
-      .filter((course) =>
-        course.mn_name.toLowerCase().includes(search.toLowerCase()),
-      )
-      .slice(0, 9);
-
-    setFilteredCourses(result);
-  };
+  const filteredCourses = courses
+    .filter((course) =>
+      course.mn_name.toLowerCase().includes(search.toLowerCase()),
+    )
+    .slice(0, 9);
 
   return (
     <div className='max-w-2xl mt-0 mx-auto w-full'>
@@ -37,12 +30,14 @@ export default function Home({ courses }) {
         <div className='relative max-w-xs md:max-w-lg w-full'>
           <input
             className={cn(
-              search ? 'rounded-t-md border-b hover:border-b-0' : 'rounded-md',
+              search
+                ? 'rounded-t-md focus:border-b-0 hover:border-b-0'
+                : 'rounded-md',
               'w-full py-2 pl-4 pr-10 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-2 border-gray-200 dark:border-gray-700 focus:outline-none focus:border-gray-400 dark:focus:border-gray-500 hover:border-gray-400 dark:hover:border-gray-500 outline-none',
             )}
             type='text'
             placeholder='Хичээлийн нэрээр хайх...'
-            onChange={(e) => filterCourse(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <div className='absolute top-0 right-0 flex items-center justify-center h-full px-2'>
             <SearchIcon className='h-5 w-5 text-gray-400' />
@@ -54,7 +49,7 @@ export default function Home({ courses }) {
             'relative inline-block max-w-xs md:max-w-lg w-full bg-white dark:bg-gray-700 rounded-b-md drop-shadow-lg overflow-hidden transition-all duration-75 ease-in py-2',
           )}
         >
-          {filteredCourses.length > 0 ? (
+          {search && filteredCourses.length ? (
             filteredCourses.map((course) => (
               <div
                 className='cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 py-2 px-4'
