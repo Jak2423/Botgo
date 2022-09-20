af27e282bd3568ccfa42f42327c3d44250d32e2c
Javkhlan <javhlan0420@gmail.com>
2022-09-20 18:42:27 +0800

    fix sticky footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,20 +2,16 @@ import React from 'react';
 
 export default function Footer() {
 	return (
-		<div className='max-w-2xl w-full mx-auto flex items-center justify-center'>
-			<div className='mt-10 mb-5'>
-				<p className='text-center text-xs md:text-sm'>
-					Designed & Developed by Jak.
-					<br />
-					Built with{' '}
-					<span className='font-semibold text-blue-500'>
-						Next.js
-					</span> &{' '}
-					<span className='font-semibold text-blue-500'>Tailwind CSS</span>
-					. Hosted on{' '}
-					<span className='font-semibold text-blue-500'>Vercel</span>.
-				</p>
-			</div>
+		<div className='max-w-2xl w-full mx-auto flex items-center justify-center absolute bottom-0 left-0 right-0 h-[32px] mb-4'>
+			<p className='text-center text-xs md:text-sm'>
+				Designed & Developed by Jak.
+				<br />
+				Built with{' '}
+				<span className='font-semibold text-blue-500'>Next.js</span> &{' '}
+				<span className='font-semibold text-blue-500'>Tailwind CSS</span>.
+				Hosted on{' '}
+				<span className='font-semibold text-blue-500'>Vercel</span>.
+			</p>
 		</div>
 	);
 }
--- components/Layout.js
+++ components/Layout.js
@@ -3,27 +3,28 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 
 export default function Layout({ children }) {
-  return (
-    <div>
-      <Head>
-        <title>Botgo</title>
-        <meta name='title' content='Botgo' />
-        <meta
-          name='keywords'
-          content='num, courses, муис хичээл, хичээл сонголт, муис, botgo'
-        />
-        <meta name='description' content='МУИС-ийн хичээлийн хайлтын систем' />
-        <meta
-          name='google-site-verification'
-          content='i8w_m0VgzzulNs1g3UM1Wkhs5vs-stgtqTkaDJFj_VA'
-        />
-        <meta name='robots' content='follow, index' />
-      </Head>
-      <Navbar />
-      <main className='flex flex-col justify-center px-8'>{children}</main>
-      <footer className='absolute bottom-0 left-0 right-0 h-[100px]'>
-        <Footer />
-      </footer>
-    </div>
-  );
+	return (
+		<>
+			<Head>
+				<title>Botgo</title>
+				<meta name='title' content='Botgo' />
+				<meta
+					name='keywords'
+					content='num, courses, муис хичээл, хичээл сонголт, муис, botgo'
+				/>
+				<meta
+					name='description'
+					content='МУИС-ийн хичээлийн хайлтын систем'
+				/>
+				<meta
+					name='google-site-verification'
+					content='i8w_m0VgzzulNs1g3UM1Wkhs5vs-stgtqTkaDJFj_VA'
+				/>
+				<meta name='robots' content='follow, index' />
+			</Head>
+			<Navbar />
+			<main className='flex flex-col justify-center px-8'>{children}</main>
+			<Footer />
+		</>
+	);
 }
