2c040ded87fd7b15e993e04b5475691434f7f315
Javkhlan <javhlan0420@gmail.com>
2022-09-21 13:52:49 +0800

    viewport-fit cover

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,37 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang='en'>
-      <Head>
-        <link href='/static/favicons/favicon.ico' rel='shortcut icon' />
-        <link href='/static/favicons/site.webmanifest' rel='manifest' />
-        <link
-          href='/static/favicons/apple-touch-icon.png'
-          rel='apple-touch-icon'
-          sizes='180x180'
-        />
-        <link
-          href='/static/favicons/favicon-32x32.png'
-          rel='icon'
-          sizes='32x32'
-          type='image/png'
-        />
-        <link
-          href='/static/favicons/favicon-16x16.png'
-          rel='icon'
-          sizes='16x16'
-          type='image/png'
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='en'>
+			<Head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, viewport-fit=cover'
+				/>
+				<link href='/static/favicons/favicon.ico' rel='shortcut icon' />
+				<link href='/static/favicons/site.webmanifest' rel='manifest' />
+				<link
+					href='/static/favicons/apple-touch-icon.png'
+					rel='apple-touch-icon'
+					sizes='180x180'
+				/>
+				<link
+					href='/static/favicons/favicon-32x32.png'
+					rel='icon'
+					sizes='32x32'
+					type='image/png'
+				/>
+				<link
+					href='/static/favicons/favicon-16x16.png'
+					rel='icon'
+					sizes='16x16'
+					type='image/png'
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
